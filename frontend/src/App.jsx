654af14c2d032fe395d67c5bd71a86439c0ea497
Dhairya <dhairyadave962@gmail.com>
2026-04-06 15:56:24 +0530

    Complete the initial setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+// import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <Routes>
+      {/* <Routes>
 
-      </Routes>
+      </Routes> */}
+
+      <h1 className='text-4xl text-indigo-500 font-poppins'>Heeloo!!!</h1>
     </>
   )
 }
