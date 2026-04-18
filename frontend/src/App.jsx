747543c161cb2563f5dd688ffbea69d083ee99a1
Dhairya <dhairyadave962@gmail.com>
2026-04-18 09:57:23 +0530

    Create routing for all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Navbar, Footer } from "./components/component_index";
-import { Home } from "./pages/page_index";
+import { Home, About, Pricing, Products, Support, Signup } from "./pages/page_index";
 
 function App() {
   return (
@@ -9,6 +9,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/" element={<About />} />
+        <Route path="/" element={<Pricing />} />
+        <Route path="/" element={<Products />} />
+        <Route path="/" element={<Support />} />
+        <Route path="/" element={<Signup />} />
       </Routes>
       <Footer />
     </>
