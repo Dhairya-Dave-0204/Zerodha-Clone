c3db34877c464e9d98d51fe5d21fae4c4fdb4fab
Dhairya <dhairyadave962@gmail.com>
2026-04-19 12:34:33 +0530

    Create the hero section for products page

--- frontend/src/components/productHero/ProductHero.jsx
+++ frontend/src/components/productHero/ProductHero.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function ProductHero() {
   return (
-    <div>ProductHero</div>
-  )
+    <div className="px-4 py-8 mt-16 sm:px-6 md:px-10 lg:px-16">
+      <div className="mx-auto text-center">
+        <h1 className="text-xl font-medium leading-tight md:text-2xl lg:text-3xl">
+          Zerodha Products
+        </h1>
+        <p className="mt-4 text-xl text-gray-600">Sleek, modern, and intuitive trading platforms</p>
+        <p className="mt-4">Check out our <span className="text-blue-700">investment offerings →</span></p>
+      </div>
+
+      <div className="w-full mx-auto mt-10 border-t border-gray-200 lg:mt-16 sm:mt-20 2xl:w-7xl" />
+    </div>
+  );
 }
 
-export default ProductHero
+export default ProductHero;
