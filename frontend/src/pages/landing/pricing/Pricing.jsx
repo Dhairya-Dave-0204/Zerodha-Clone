2c0a13b9260751687875903ae530244e6d02b4b7
Dhairya <dhairyadave962@gmail.com>
2026-04-27 11:14:46 +0530

    Add the cta section

--- frontend/src/pages/landing/pricing/Pricing.jsx
+++ frontend/src/pages/landing/pricing/Pricing.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { PricingHero, Brokerage } from "../../../components/component_index"
+import { PricingHero, Brokerage, OpenAccount } from "../../../components/component_index"
 
 function Pricing() {
   return (
     <>
       <PricingHero />
       <Brokerage />
+      <OpenAccount />
     </>
   )
 }
