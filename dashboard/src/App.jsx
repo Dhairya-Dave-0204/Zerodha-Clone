d9b52bd58d8e3889a68e6a9b47c80130e560affe
Dhairya <dhairyadave962@gmail.com>
2026-04-27 13:58:23 +0530

    Create the basic config of the app and tailwind

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <h1>HElLo</h1>
+      <h1 className="text-5xl text-primary">HElLo</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
