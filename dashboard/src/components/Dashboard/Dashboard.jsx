c94cbdbb1036d69c4f75b91ddfc44739ffb45183
Dhairya <dhairyadave962@gmail.com>
2026-05-15 16:37:52 +0530

    Create the route setup for the dashboard

--- dashboard/src/components/Dashboard/Dashboard.jsx
+++ dashboard/src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import {
+  WatchList,
+  Summary,
+  Orders,
+  Holdings,
+  Positions,
+  Funds,
+  Apps,
+} from "../component_index";
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+    <WatchList />
+      <div>
+        <Routes>
+          <Route path="/" element={<Summary />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/holdings" element={<Holdings />} />
+          <Route path="/positions" element={<Positions />} />
+          <Route path="/funds" element={<Funds />} />
+          <Route path="/apps" element={<Apps />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
