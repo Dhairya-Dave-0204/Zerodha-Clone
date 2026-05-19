18ae59ca5c3bef88ad7e805ee4ecd780fc218dee
Dhairya <dhairyadave962@gmail.com>
2026-05-19 16:12:21 +0530

    Create the setup to run the server

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import { connectDB } from "./db/db.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed in server file!", err);
+  });
