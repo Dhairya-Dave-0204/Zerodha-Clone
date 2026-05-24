a48971a8327e3b5a68f889083b1a0e670962ceab
Dhairya <dhairyadave962@gmail.com>
2026-05-24 15:42:11 +0530

    Create the controller to get all holdings

--- backend/src/controllers/holdings.controller.js
+++ backend/src/controllers/holdings.controller.js
@@ -1,7 +1,20 @@
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Holding } from "../models/holdings.model.js";
 
-const getAllHoldings = asyncHandler(async () => {});
+const getAllHoldings = asyncHandler(async (req, res) => {
+  let allHoldings = await Holding.find();
+
+  if (!allHoldings.length > 0) {
+    throw new ApiError(401, "Error in fetching all the holdings");
+  }
+
+  res
+    .status(200)
+    .send(
+      new ApiResponse(200, allHoldings, "All holdings fetched successfully"),
+    );
+});
 
 export { getAllHoldings };
