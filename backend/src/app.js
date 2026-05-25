dad6d479b504cc1f76cb6dd8f80c13b3de4dea98
Dhairya <dhairyadave962@gmail.com>
2026-05-25 11:17:02 +0530

    Configure the cookie parser in the app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { ApiError } from "./utils/ApiError.js";
 
 const app = express();
@@ -21,6 +22,8 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
   res.send("Hello");
