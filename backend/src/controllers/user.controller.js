d8dd30c78b76d6625d2dfe8cd0f40a1b080eb12b
Dhairya <dhairyadave962@gmail.com>
2026-05-26 16:21:39 +0530

    Create the controller to login the user

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,18 +1,49 @@
 import mongoose from "mongoose";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { User } from "../models/user.model.js"
+import { User } from "../models/user.model.js";
 
-const registerUser = asyncHandler( async (req, res) => {} )
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, fullname, password } = req.body;
 
-const loginUser = asyncHandler(async (req, res) => {})
+  if (
+    [username, email, password, fullname].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
 
-const logoutUser = asyncHandler(async (req, res) => {})
+  const existingUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
 
-export {
-    registerUser,
-    loginUser,
-    logoutUser
-}
+  if (existingUser) {
+    throw new ApiError(409, "User already exists with same email or username");
+  }
+
+  const user = await User.create({
+    username: username.toLowerCase(),
+    email,
+    fullname,
+    password,
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken",
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Server error; Registration unsuccessful");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, createdUser, "Registration successful"));
+});
+
+const loginUser = asyncHandler(async (req, res) => {});
+
+const logoutUser = asyncHandler(async (req, res) => {});
+
+export { registerUser, loginUser, logoutUser };
