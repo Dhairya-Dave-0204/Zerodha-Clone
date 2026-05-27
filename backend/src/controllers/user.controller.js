9f181131b9d5298f581d2562c795d2a83e9af041
Dhairya <dhairyadave962@gmail.com>
2026-05-27 15:01:07 +0530

    Create controller to logout user

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -89,7 +89,30 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-const logoutUser = asyncHandler(async (req, res) => {});
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset: {
+        refreshToken: 1,
+      },
+    },
+    {
+      new: true,
+    },
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
