77e0e85565ff16f6acf22aabc25c4c3d058db6ec
Dhairya <dhairyadave962@gmail.com>
2026-05-30 16:15:47 +0530

    Create the routes to handle user operations

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,20 @@
 import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js"
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getCurrentUser,
+  refreshAccessToken,
+} from "../controllers/user.controller.js";
 
-const userRouter = Router()
+const userRouter = Router();
 
-export default userRouter
+userRouter.route("/register").post(registerUser)
+userRouter.route("/login").post(loginUser)
+
+userRouter.route("/logout").post(verifyJWT, logoutUser)
+userRouter.route("/current-user").get(verifyJWT, getCurrentUser)
+userRouter.route("/refresh-token").post(refreshAccessToken)
+
+export default userRouter;
