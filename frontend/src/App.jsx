c7433353263ccf47b648cdc563374450aa868c04
Dhairya <dhairyadave962@gmail.com>
2026-05-30 18:17:14 +0530

    Create the customized toast cofig using react hot toast]"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,51 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Navbar, Footer } from "./components/component_index";
-import { Home, About, Pricing, Products, Support, Signup, NotFound } from "./pages/page_index";
+import {
+  Home,
+  About,
+  Pricing,
+  Products,
+  Support,
+  Signup,
+  NotFound,
+} from "./pages/page_index";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={10}
+        toastOptions={{
+          duration: 4000,
+
+          style: {
+            background: "#ffffff",
+            color: "#424242",
+            border: "1px solid #e5e7eb",
+            padding: "14px 18px",
+            fontSize: "14px",
+            borderRadius: "6px",
+          },
+
+          success: {
+            iconTheme: {
+              primary: "#387ed1",
+              secondary: "#fff",
+            },
+          },
+
+          error: {
+            iconTheme: {
+              primary: "#ef4444",
+              secondary: "#fff",
+            },
+          },
+        }}
+      />
       <Navbar />
       <Routes>
         <Route path="*" element={<NotFound />} />
