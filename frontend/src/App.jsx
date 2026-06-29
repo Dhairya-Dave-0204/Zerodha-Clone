ba99642ac4802c5aeac764b51d4defb324a443ef
Dhairya Dave <dhairyadave962@gmail.com>
2026-06-29 15:03:39 +0530

    Incorporate lazy loading for optimization and perfomance enhancement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
 import "./App.css";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Navbar, Footer } from "./components/component_index";
-import {
-  Home,
-  About,
-  Pricing,
-  Products,
-  Support,
-  NotFound,
-  SignUp,
-  SignIn,
-} from "./pages/page_index";
+import { NotFound } from "./pages/page_index";
 import { Toaster } from "react-hot-toast";
+
+import PageLoader from "./components/PageLoader/PageLoader";
+
+// Lazy Loaded Pages
+const Home = lazy(() => import("./pages/landing/home/Home"));
+const About = lazy(() => import("./pages/landing/about/About"));
+const Pricing = lazy(() => import("./pages/landing/pricing/Pricing"));
+const Products = lazy(() => import("./pages/landing/products/Products"));
+const Support = lazy(() => import("./pages/landing/support/Support"));
+const SignUp = lazy(() => import("./pages/SignUp/SignUp"));
+const SignIn = lazy(() => import("./pages/SignIn/SignIn"));
 
 function App() {
   return (
@@ -47,17 +50,22 @@ function App() {
           },
         }}
       />
+
       <Navbar />
-      <Routes>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/support" element={<Support />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-      </Routes>
+
+      <Suspense fallback={<PageLoader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/support" element={<Support />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+
       <Footer />
     </>
   );
